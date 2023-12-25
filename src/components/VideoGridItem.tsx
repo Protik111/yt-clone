c2769ee7e10c978e0d0572d071d3e030d098d62a
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2023-12-25 17:36:08 +0600

    [DONE] video grid item

---
+++ src/components/VideoGridItem.tsx
@@ -0,0 +1,43 @@
+type VideoGridItemProps = {
+  id: string;
+  title: string;
+  channel: {
+    id: string;
+    name: string;
+    profileUrl: string;
+  };
+  views: number;
+  postedAt: Date;
+  duration: number;
+  thumbnailUrl: string;
+  videoUrl: string;
+};
+
+const VideoGridItem = ({
+  id,
+  title,
+  channel,
+  views,
+  postedAt,
+  duration,
+  thumbnailUrl,
+  videoUrl,
+}: VideoGridItemProps) => {
+  return (
+    <div className="flex flex-col gap-2">
+      <a href={`/watch/${id}`} className="relative aspect-video">
+        <img
+          src={thumbnailUrl}
+          alt={title}
+          className="block h-full w-full object-cover rounded-xl"
+        />
+
+        <div className="absolute bottom-1 right-1 bg-secondary-dark text-secondary text-sm px-0.5 rounded">
+          {duration}
+        </div>
+      </a>
+    </div>
+  );
+};
+
+export default VideoGridItem;
