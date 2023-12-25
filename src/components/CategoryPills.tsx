630c6b8cc4227939458de607b27d68eb068e9c12
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2023-12-25 13:36:04 +0600

    [DONE] cateogy pills left and right icon design

--- src/components/CategoryPills.tsx
+++ src/components/CategoryPills.tsx
@@ -1,4 +1,6 @@
+import { ChevronLeft, ChevronRight } from "lucide-react";
 import Button from "./Button";
+import { useState } from "react";
 
 type CategoryPillProps = {
   categories: string[];
@@ -11,6 +13,8 @@ const CategoryPills = ({
   selectedCategory,
   onSelect,
 }: CategoryPillProps) => {
+  const [isLeftVisible, setIsLeftVisible] = useState(true);
+  const [isRightVisible, setIsRightVisible] = useState(true);
   return (
     <div className="overflow-x-hidden relative">
       <div className="flex gap-3 whitespace-nowrap transition-transform w-[max-content]">
@@ -25,6 +29,29 @@ const CategoryPills = ({
           </Button>
         ))}
       </div>
+      {isLeftVisible && (
+        <div className="absolute left-0 top-1/2 -translate-y-1/2 bg-gradient-to-r from-white from-50% to-transparent w-24 h-full">
+          <Button
+            variant="ghost"
+            size="icon"
+            className="h-full aspect-square w-auto p-1.5"
+          >
+            <ChevronLeft />
+          </Button>
+        </div>
+      )}
+
+      {isRightVisible && (
+        <div className="absolute right-0 top-1/2 -translate-y-1/2 bg-gradient-to-l from-white from-50% to-transparent w-24 h-full flex justify-end">
+          <Button
+            variant="ghost"
+            size="icon"
+            className="h-full aspect-square w-auto p-1.5"
+          >
+            <ChevronRight />
+          </Button>
+        </div>
+      )}
     </div>
   );
 };
