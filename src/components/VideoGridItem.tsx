5d9587346fe4b77e44d3d05086024ca7d8f11ac7
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2023-12-27 08:56:27 +0600

    [DONE] video hover animation

--- src/components/VideoGridItem.tsx
+++ src/components/VideoGridItem.tsx
@@ -46,17 +46,18 @@ const VideoGridItem = ({
   }, [isVideoPlaying]);
 
   return (
-    <div className="flex flex-col gap-2">
+    <div className="flex flex-col gap-2" onMouseEnter={() => setIsVideoPlaying(true)} onMouseLeave={() => setIsVideoPlaying(false)}>
       <a href={`/watch/${id}`} className="relative aspect-video">
         <img
           src={thumbnailUrl}
           alt={title}
-          className="block h-full w-full object-cover rounded-xl"
+          className={`block h-full w-full object-cover transition-[border-radius] duration-200 rounded-xl ${isVideoPlaying ? "rounded-none" : "rounded-xl"}`}
         />
 
         <div className="absolute bottom-1 right-1 bg-secondary-dark text-secondary text-sm px-0.5 rounded">
           {formatDuration(duration)}
         </div>
+        <video className={`block h-full object-cover absolute inset-0 transition-opacity duration-200 ${isVideoPlaying ? "opacity-100 delay-100" : 'opacity-0'}`} ref={videoRef} muted playsInline src={videoUrl} />
       </a>
 
       <div className="flex gap-2">
